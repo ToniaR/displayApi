4ce069100dfc0c622771ac00dab28494596f5b2a
Antanina <antosia535@gmail.com>
2022-06-08 19:41:56 +0200

    Display items in list

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,17 +1,30 @@
 <script lang="ts">
 import axios from "axios"
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref, reactive } from 'vue'
+
+interface Item {
+  API: string,
+  Auth: string,
+  Category: string,
+  Cors: string,
+  Description: string,
+  HTTPS: boolean
+  Link: string
+}
+
+type Example = {
+  entries: Item[];
+};
 
 export default defineComponent({
   setup() {
-    const data = ref({});
+    let items = ref<Item[]>([]);
 
     const getData = () => {
       axios.get('https://api.publicapis.org/entries')
       .then((response) => {
-          const { entries } = response.data;
-          console.log({entries});
-          data.value = entries;
+          const { entries }: Example  = response.data;
+          items.value = entries;
       })
     }
 
@@ -19,17 +32,30 @@ export default defineComponent({
       console.log('component did mounted');
       getData();
     })
-    return { data };
+    return { items };
 
-  }
-
-
-  
+  }  
 })
+
 </script>
 
 <template>
   <h1>Items</h1>
+
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      <p><strong>title: </strong>{{ item.API }}</p>
+      <p><strong>token: </strong>{{ item.Auth }}</p>
+      <p><strong>category: </strong>{{ item.Category }}</p>
+      <p><strong>is cors available: </strong>{{ item.Cors }}</p>
+      <p><strong>description: </strong>{{ item.Description }}</p>
+      <p><strong>https available: </strong>{{ item.HTTPS }}</p>
+      <p><a :href="item.Link" target="blank">Go the the documentation</a></p>
+      <hr/>
+    </li>
+  </ul>
+
+
   
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue'
 import ItemsList from '@/components/ItemsList.vue'
 
-defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
